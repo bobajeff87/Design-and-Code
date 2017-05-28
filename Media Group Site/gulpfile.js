ed5b094898c3d882e706855b1b0ac20df8abad50
boba_jeff <jmarsden1987@gmail.com>
2017-05-27 18:51:46 -0700

    gulp setup

--- Media Group Site/gulpfile.js
+++ Media Group Site/gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-    console.log('hello world')
+// uglify js
+
+gulp.task('scripts', function() {
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('minifyjs'))
 });
+
+gulp.task('prefixer', function() {
+    console.log('fucking work damn it!!')
+});
+
+gulp.task('default', ['scripts', 'prefixer']);
