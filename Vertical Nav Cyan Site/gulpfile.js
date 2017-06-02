e4dda26db542c59405e5dcc04f771ec197d3d2c8
bobajeff87 <jmarsden1987@gmail.com>
2017-06-02 12:55:46 -0700

    cyan site project set-up

---
+++ Vertical Nav Cyan Site/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var browsersync = require('browser-sync').create();
+var uglifycss = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+
+// uglify js
+
+gulp.task('scripts', function() {
+    gulp.src('js/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('minifyjs'))
+});
+
+// css prefixer
+
+gulp.task('styles', function() {
+    gulp.src('css/styles.css')
+        .pipe(plumber())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('CSSprefixed'))
+        .pipe(browsersync.reload({ stream: true }))
+});
+
+// uglify css
+
+gulp.task('cssuglify', function() {
+    return gulp.src('css/styles.css')
+        .pipe(uglifycss())
+        .pipe(gulp.dest('CSSuglify'))
+});
+
+// browser-sync
+
+gulp.task('browsersync', function() {
+    browsersync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('css/styles.css', ['styles']);
+    gulp.watch('index.html').on('change', broswersync.reload({ stream: true }));
+});
+
+gulp.task('default', ['styles', 'cssuglify', 'scripts', 'browsersync']);
