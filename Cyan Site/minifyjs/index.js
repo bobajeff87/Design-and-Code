1631b83c303f4bf618450ed589165b18c91bdde7
bobajeff7 <jmarsden1987@gmail.com>
2017-10-24 14:32:37 -0700

    synchronize change

--- Cyan Site/minifyjs/index.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".main-menu").hover(function(){$(".main-drop-menu").css("visibility","visible")}),$(".main-menu").mouseleave(function(){$(".main-drop-menu").css("visibility","hidden")}),$(".work-about").hover(function(){$(".work-about-drop").css("visibility","visible")}),$(".work-about").mouseleave(function(){$(".work-about-drop").css("visibility","hidden")})});
