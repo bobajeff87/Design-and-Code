d3b4542d5020ef3dcc57cf9f9f26900970e47c89
bobajeff7 <jmarsden1987@gmail.com>
2018-01-06 16:17:10 -0800

    add animate

---
+++ Web_App/src/index.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+    $(".wonderwoman").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".wonderwoman").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".deadalive").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".deadalive").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".thor").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".thor").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".nightmare").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".nightmare").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".superbad").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".superbad").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".avengers3").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".avengers3").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".guardians2").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".guardians2").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".theory").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".theory").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+    $(".spiderman").mouseenter(function() {
+        $(this).addClass("animated pulse");
+    });
+
+    $(".spiderman").mouseleave(function() {
+        $(this).removeClass("animated pulse");
+    });
+
+});
