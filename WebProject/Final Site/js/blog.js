af0ed916340852ef5dee338a99cd0d7a328c302b
bobajeff7 <jmarsden1987@gmail.com>
2018-01-16 17:38:51 -0800

    completing slider

--- WebProject/Final Site/js/blog.js
+++ WebProject/Final Site/js/blog.js
@@ -1,18 +1,89 @@
-var slide_index = 1;  
-        displaySlides(slide_index); 
+$(document).ready(function() {
 
-        function nextSlide(n) {  
-            displaySlides(slide_index += n);  
-        }  
-        function currentSlide(n) {  
-            displaySlides(slide_index = n);  
-        }  
-        function displaySlides(n) {  
-            var i;  
-            var slides = document.getElementsByClassName("showSlide");  
-            if (n > slides.length) {slide_index = 1;} 
-            if (n < 1) {slide_index = slides.length;} 
-            for (i = 0; i < slides.length; i++) {  
-                slides[i].style.display = "none";  
-            }  
-            slides[slide_index - 1].style.display = "block";}
+    $(".design").hover(function() {
+        $(".designpopup").css("visibility", "visible");
+    });
+
+    $(".design").mouseleave(function() {
+        $(".designpopup").css("visibility", "hidden");
+    });
+
+    $(".photography").hover(function() {
+        $(".photopopup").css("visibility", "visible");
+    });
+
+    $(".photography").mouseleave(function() {
+        $(".photopopup").css("visibility", "hidden");
+    });
+
+    $(".branding").hover(function() {
+        $(".brandingpopup").css("visibility", "visible");
+    });
+
+    $(".branding").mouseleave(function() {
+        $(".brandingpopup").css("visibility", "hidden");
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    function sslider() {
+      
+      var current = 0,
+          i,
+          slider = document.querySelector('[data-js="sslide"]'),
+          allImages =  slider.querySelectorAll('img'),
+          imgWidth = Math.ceil(100 / allImages.length),
+          sliderWidth = allImages.length * 100;
+      
+      slider.style.width = sliderWidth + '%';
+      
+      for(i = 0; i <= allImages.length - 1; i++) {
+         allImages[i].style.width = imgWidth + '%';
+      }
+  
+    function animateRight(cur) {
+        var i = imgWidth,
+            time = 50;
+        var animate = setInterval(function() {
+        if(i <= sliderWidth) {
+          allImages[cur].style.marginLeft = "-" + i + "%";
+          i--;
+        } else {
+          clearInterval(animate);
+        }
+        }, time);  
+     } 
+      
+      function reset() {
+        for(i = 0; i <= allImages.length - 1; i++) {
+          animateRight(i);
+        }
+        // resseting the current image to the first image
+        current = 0;
+      }    
+      
+      function animateLeft(cur) {
+        var i = 0,
+            time = 50;
+        var animate = setInterval(function() {
+        if(i <= imgWidth) {
+          allImages[cur].style.marginLeft = "-" + i  + "%";
+          i++;
+        } else {
+          clearInterval(animate);
+        }
+        }, time);  
+     }
+      
+      setInterval(function () {
+        if(current <= allImages.length - 2) {
+          animateLeft(current);
+          current++;
+          
+        } else {
+          reset();
+        }
+      }, 3000);
+  }
+   sslider();
+  });
