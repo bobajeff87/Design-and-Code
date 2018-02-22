cb308ddd3d350c0984cc6d2a16f31f9635d37da2
bobajeff7 <jmarsden1987@gmail.com>
2018-02-22 13:57:14 -0800

    reactjs interface

---
+++ Buiding an Interface with ReactJs/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    webserver = require('gulp-webserver');
+
+var src = './process',
+    app = '.builds/app';
+
+gulp.task('js', function() {
+    return gulp.src( src + '/js/app.js' )
+    .pipe(browserify({
+        transform: 'reactify',
+        debug: true
+    }))
+    .on('error', function (err) {
+        console.error('Error!', err.message);
+    })
+    .pipe(gulp.dest(app + '/js'));
+});
+
+gulp.task('html', function () {
+    gulp.src( app + '/**/*.html';)
+});
