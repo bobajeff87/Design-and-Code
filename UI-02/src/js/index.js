f03ceee269746ecccd575a6b0d33a9f636e58f20
bobajeff87 <jmarsden1987@gmail.com>
2018-10-06 14:06:28 -0700

    added jquery menu

---
+++ UI-02/src/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $(".glyphicon-list").mouseenter(function() {
+        $(".begonia-drop").show();
+    });
+    $(".glyphicon-list").mouseleave(function() {
+        $(".begonia-drop").hide();
+    });
+});
+
