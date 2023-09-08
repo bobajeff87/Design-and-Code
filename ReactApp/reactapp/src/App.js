798f669e7723707c7c33cd866492895c8ec106fe
bobajeff87 <jmarsden1987@gmail.com>
2023-09-08 18:34:12 -0400

    testing

--- ReactApp/reactapp/src/App.js
+++ ReactApp/reactapp/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <h1>This is a Header!</h1>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+      </ul>
     </div>
   );
 }
